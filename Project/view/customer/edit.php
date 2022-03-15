115d2f62881e53f103c47eb9590c396cb78b3944
DhruvP2205 <prajapatidhruv789@gmail.com>
2022-03-15 13:37:47 +0530

    Task Uploaded

--- Project/view/customer/edit.php
+++ Project/view/customer/edit.php
@@ -41,50 +41,51 @@ $shippingAddress = $customer->getShippingAddress(); ?>
             <input type="text" name="billingAddress[customerId]" value="<?php echo $billingAddress->customerId ?>" hidden>
             <input type="text" name="billingAddress[addressId]" value="<?php echo $billingAddress->addressId ?>" hidden>
             <td>Address</td>
-            <td><textarea name="billingAddress[address]" rows="4" cols="50"><?php echo $billingAddress->address; ?></textarea></td>
+            <td><textarea name="billingAddress[address]" id="billingAddress[address]" rows="4" cols="50"><?php echo $billingAddress->address; ?></textarea></td>
         </tr>
         <tr>
             <td>Zip code</td>
-            <td><input type="text" name="billingAddress[zipcode]" value="<?php echo $billingAddress->zipcode; ?>"></td>
+            <td><input type="text" name="billingAddress[zipcode]" id="billingAddress[zipcode]" value="<?php echo $billingAddress->zipcode; ?>"></td>
         </tr>
         <tr>
             <td>City</td>
-            <td><input type="text" name="billingAddress[city]" value="<?php echo $billingAddress->city; ?>"></td>
+            <td><input type="text" name="billingAddress[city]" id="billingAddress[city]" value="<?php echo $billingAddress->city; ?>"></td>
         </tr>
         <tr>
             <td>State</td>
-            <td><input type="text" name="billingAddress[state]" value="<?php echo $billingAddress->state; ?>"></td>
+            <td><input type="text" name="billingAddress[state]" id="billingAddress[state]" value="<?php echo $billingAddress->state; ?>"></td>
         </tr>
         <tr>
             <td>Country</td>
-            <td><input type="text" name="billingAddress[country]" value="<?php echo $billingAddress->country; ?>"></td>
+            <td><input type="text" name="billingAddress[country]" id="billingAddress[country]" value="<?php echo $billingAddress->country; ?>"></td>
         </tr>
         <input type="hidden" name="billingAddress[billingAddress]" value="1">
         <input type="hidden" name="billingAddress[shipingAddress]" value="2">
         <tr>
             <td colspan="2"><h3>Shiping Address:</h3></td>
         </tr>
+        <tr><td colspan="2"><input type="checkbox" name="copyAddress" id="copyAddress" onchange="sameAddress()" /> Same as Billing Address</td></tr>
         <tr>
             <input type="text" name="shippingAddress[customerId]" value="<?php echo $shippingAddress->customerId ?>" hidden>
             <input type="text" name="shippingAddress[addressId]" value="<?php echo $shippingAddress->addressId ?>" hidden>
             <td>Address</td>
-            <td><textarea name="shippingAddress[address]" rows="4" cols="50"><?php echo $shippingAddress->address; ?></textarea></td>
+            <td><textarea name="shippingAddress[address]" id="shippingAddress[address]" rows="4" cols="50"><?php echo $shippingAddress->address; ?></textarea></td>
         </tr>
         <tr>
             <td>Zip code</td>
-            <td><input type="text" name="shippingAddress[zipcode]" value="<?php echo $shippingAddress->zipcode; ?>"></td>
+            <td><input type="text" name="shippingAddress[zipcode]" id="shippingAddress[zipcode]" value="<?php echo $shippingAddress->zipcode; ?>"></td>
         </tr>
         <tr>
             <td>City</td>
-            <td><input type="text" name="shippingAddress[city]" value="<?php echo $shippingAddress->city; ?>"></td>
+            <td><input type="text" name="shippingAddress[city]" id="shippingAddress[city]" value="<?php echo $shippingAddress->city; ?>"></td>
         </tr>
         <tr>
             <td>State</td>
-            <td><input type="text" name="shippingAddress[state]" value="<?php echo $shippingAddress->state; ?>"></td>
+            <td><input type="text" name="shippingAddress[state]" id="shippingAddress[state]" value="<?php echo $shippingAddress->state; ?>"></td>
         </tr>
         <tr>
             <td>Country</td>
-            <td><input type="text" name="shippingAddress[country]" value="<?php echo $shippingAddress->country; ?>"></td>
+            <td><input type="text" name="shippingAddress[country]" id="shippingAddress[country]" value="<?php echo $shippingAddress->country; ?>"></td>
         </tr>
         <input type="hidden" name="shippingAddress[billingAddress]" value="2">
         <input type="hidden" name="shippingAddress[shipingAddress]" value="1">
@@ -95,6 +96,28 @@ $shippingAddress = $customer->getShippingAddress(); ?>
                 <button type="button"><a href="<?php echo $this->getUrl('grid','customer',[],true) ?>">Cancel</a></button>
             </td>
         </tr>
-        
-    </table>    
+    </table> 
+
+    <script type="text/javascript">
+        function sameAddress()
+        {
+            var checkedBox = document.getElementById("copyAddress");
+            
+            if(checkedBox.checked == true){
+                document.getElementById("shippingAddress[address]").value = document.getElementById("billingAddress[address]").value; 
+                document.getElementById("shippingAddress[zipcode]").value = document.getElementById("billingAddress[zipcode]").value; 
+                document.getElementById("shippingAddress[city]").value = document.getElementById("billingAddress[city]").value; 
+                document.getElementById("shippingAddress[state]").value = document.getElementById("billingAddress[state]").value; 
+                document.getElementById("shippingAddress[country]").value = document.getElementById("billingAddress[country]").value; 
+            }
+            else
+            {
+                    document.getElementById("shippingAddress[address]").value = null; 
+                    document.getElementById("shippingAddress[zipcode]").value = null; 
+                    document.getElementById("shippingAddress[city]").value = null; 
+                    document.getElementById("shippingAddress[state]").value = null; 
+                    document.getElementById("shippingAddress[country]").value = null; 
+            }
+    }
+    </script>
 </form>
